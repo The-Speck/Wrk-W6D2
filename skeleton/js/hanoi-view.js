d1dffb5390916543cc3f21e0c56f459279b32fbc
Alex & Jackie <a.plyushchay@gmail.com>
2018-06-26 17:39:40 -0400

    Towers of Hanoi

--- skeleton/js/hanoi-view.js
+++ skeleton/js/hanoi-view.js
@@ -5,11 +5,33 @@ class HanoiView {
     
     this.setupTowers();
     this.render();
+    
+    let isFirstMove = true;
+    let firstMove;
+    
+    $('ul').on('click', (e) => {
+      if (isFirstMove) {
+        const $divEl = $(e.currentTarget).find('div');
+        $divEl.css('background', 'red');
+        
+        isFirstMove = false;
+        firstMove = $(e.currentTarget).data('tower');
+      } else {
+        $('div').css('background', 'black');
+        isFirstMove = true;
+        
+        let secondMove = $(e.currentTarget).data('tower');
+        this.game.move(firstMove, secondMove);
+        firstMove = null;
+      }
+      this.render();
+    });
   }
   
   setupTowers(){
     for (let i = 0; i < 3; i++) {
       const towers = $('<ul></ul>');
+      towers.data('tower', i);
       
       for (let j = 0; j < 3; j++){
         const $disc = $('<li></li>');
@@ -21,7 +43,8 @@ class HanoiView {
       this.$el.append(towers);
     }
     const $underline = $('<div></div>');
-    $('ul').$el.append($underline);
+    $underline.addClass('underline');
+    $('ul').append($underline);
   }
   
   render() {
@@ -31,7 +54,7 @@ class HanoiView {
     $('li').removeClass('disc3');
     
     for( let i = 0; i < 3; i++) {
-      if (towers[i].reverse().length > 0) {
+      if (towers[i].length > 0) {
         for (let j = 0; j < towers[i].length; j++) {
           let $tower = $(`.pos${i}${j}`);
           $tower.addClass(`disc${towers[i][j]}`);
